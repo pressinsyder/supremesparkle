024d4bda99deb78e329f0db8af6af787ad88de7f
Justine Gichana <justinequartz@gmail.com>
2021-02-19 09:30:37 +0300

    title change

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Supreme Sparkle!</a>
+          Welcome to <a href="https://nextjs.org">Supreme Sparkle adn Justines career programming interface</a>
         </h1>
 
         <p className={styles.description}>
